c20a0c3e96e7639f459ebfd2013e67550575aa29
Parthib Pal <101968440+ParthibPal@users.noreply.github.com>
2026-08-07 02:00:17 +0000

    try_catch_code

---
+++ Python/01_try_except.py
@@ -0,0 +1,45 @@
+# # try except block example
+# try:
+#     print("Papa")
+# except:
+#     print("An exception occurs")
+
+
+# # Basic try catch block (ZeroDivisionError)
+# try:
+#     num = int(input("Enter a number: "))
+#     print(100/num)
+# except ZeroDivisionError:
+#     print("Can not divide by zero")
+
+
+# # Multiple try catch block (ValueError/ZeroDivisionError)
+# try:
+#     num = int(input("Entre a number: "))
+#     print(100/num)
+# except ValueError:
+#     print("Enter a valid number")
+# except ZeroDivisionError:
+#     print("Can not divide by zero")
+
+
+# Capturing the exception object
+# try:
+#     num = int(input("Enter number: "))
+#     print(10 / num)
+# except Exception as e:
+#     print("Error occured: ",e)
+
+
+# Multiple Exceptions in One Block with else block
+# try:
+#     num = int(input("Enter number: "))
+#     result = 100 / num
+# except(ZeroDivisionError, ValueError) as e:
+#     print("Error occured: ",e)
+# else:
+#     print("Result: ",result)
+# finally:
+#     print("Program executed sucessfully.")
+
+
